3cf8b8b27e1673be9a3d8660b1362d2f7faeeb20
Ivo Pischner <i-pischner@ti.com>
2018-10-26 20:52:43 +0200

    Added some test cases

---
+++ test/output/vba/VBAClass3.cs
@@ -0,0 +1,29 @@
+/**
+ * @file VBAClass3.cls
+ * \brief Single VB6 class module, defining VBAClass3
+ */
+namespace vba {
+	 /**
+	 *  <summary>
+	 *  VBA class example
+	 *  </summary>
+	 *  \file VBAClass3.cls
+	 *  \remarks The original unfiltered source of this file: <A HREF="https://github.com/sevoku/doxygen-vb-filter/blob/master/samples/ClassicVB/VBAClass3.cls">VBAClass3.cls</A>
+	 */
+	class VBAClass3
+	{
+		private struct SampleType1
+		{
+			long typeItem1;
+			double typeItem2;
+		}
+		private enum SampleEnum
+		{
+			[_First] = 0,
+			enumItem1 = 0,
+			enumItem2 = 1,
+			enumItem3 = 2,
+			[_Last] = 2
+		}
+	}
+}
---
+++ test/output/vba/VBAClass4.cs
@@ -0,0 +1,29 @@
+/**
+ * @file VBAClass4.cls
+ * \brief Single VB6 class module, defining VBAClass4
+ */
+namespace vba {
+	 /**
+	 *  <summary>
+	 *  VBA class example
+	 *  </summary>
+	 *  \file VBAClass4.cls
+	 *  \remarks The original unfiltered source of this file: <A HREF="https://github.com/sevoku/doxygen-vb-filter/blob/master/samples/ClassicVB/VBAClass4.cls">VBAClass4.cls</A>
+	 */
+	class VBAClass4
+	{
+		private enum SampleEnum
+			[_First] = 0,
+			enumItem1 = 0,
+			enumItem2 = 1,
+			enumItem3 = 2,
+			[_Last] = 2
+		}
+	{
+		private struct SampleType1
+		{
+			long typeItem1;
+			double typeItem2;
+		}
+	}
+}
