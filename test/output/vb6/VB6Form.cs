94b000dfde2eef80ccdfdc847f332a310bbc731c
Ivo Pischner <i-pischner@ti.com>
2018-10-26 17:33:12 +0200

    Added test suite

---
+++ test/output/vb6/VB6Form.cs
@@ -0,0 +1,28 @@
+/**
+ * @file VB6Form.frm
+ * \brief Single VB6 class module, defining VB6Form
+ */
+namespace vb6 {
+	 /**
+	 *  \brief VB6 Form Example
+	 *  \file VB6Form.frm
+	 *  \remarks The original unfiltered source of this file: <A HREF="http://trac.sevo.org/projects/doxyvb/browser/trunk/samples/ClassicVB/VB6Form.frm">VB6Form.frm</A>
+	 */
+	class VB6Form
+	{
+		/**
+		 *  \brief Button Click handler
+		 */
+		private void Command1_Click();
+		/**
+		 *  \brief Form OnLoad handler
+		 *  \remarks Is called when the form is loading
+		 */
+		private void Form_Load();
+		/**
+		 *  \brief Text Change handler
+		 *  \remarks Is called when the text of Text1 is changed
+		 */
+		private void Text1_Change();
+	}
+}
---
+++ test/output/vb6/VB6Module.cs
@@ -0,0 +1,100 @@
+/**
+ * @file VB6Module.bas
+ *  <summary>VB6 Module Example</summary>
+ *  <remarks>The original unfiltered source of this file: <A HREF="http://trac.sevo.org/projects/doxyvb/browser/trunk/samples/ClassicVB/VB6Module.bas">VB6Module.bas</A></remarks>
+ */
+namespace vb6 {
+	/** Is imported from extern library: kernel32.dll */
+	public extern  long GetTickCount();
+	/** Is imported from extern library: kernel32 */
+	public extern  long WritePrivateProfileStringA(string strSection, string strKey, string strString, string strFileNameName);
+	/**
+	 *  Global variable example
+	 */
+	public double gl_test;
+	/**
+	 *  <summary>
+	 *  simple VB6 Public Enum
+	 *  </summary>
+	 */
+	public enum SampleEnum1
+	{
+		[_First] = 1,
+		/**
+		 *  First enum item
+		 */
+		First_Item = 1,
+		Second_Item = 2, /**< \brief Second enum item */
+		Third_Item = 3, /**< \brief Third enum item */
+		[_Last] = 3
+	}
+	/**
+	 *  <summary>
+	 *  simple VB6 Private Enum
+	 *  </summary>
+	 */
+	private enum SampleEnum2
+	{
+		[_First] = 1,
+		First_Item = 1, /**< \brief First enum item */
+		/**
+		 *  Second emum item
+		 */
+		Second_Item = 2,
+		Third_Item = 3,
+		/**
+		 *  Fourth enum item
+		 */
+		Fourth_Item = 4,
+		[_Last] = 4
+	}
+	/**
+	 *  <summary>
+	 *  simple VB6 Structure
+	 *  </summary>
+	 */
+	public struct SampleVB6Structure
+	{
+		/**
+		 *  simple private integer value
+		 */
+		private int someInteger;
+		/**
+		 *  simple public string value
+		 */
+		public string someString;
+		public long someLong;
+	}
+	/**
+	 *  <summary>
+	 *  simple VBA/VB6 Type
+	 *  </summary>
+	 */
+	public struct SampleVBAType
+	{
+		/**
+		 *  simple integer value
+		 */
+		/**
+		 *  simple single value
+		 */
+	}
+	/**
+	 *  <summary>
+	 *  function with parameter
+	 *  </summary>
+	 *  <param name="pFirst">simple parameter</param>
+	 *  <returns>some double value</returns>
+	 *  <remarks>Test remark</remarks>
+	 */
+	public  double SampleModuleFunction(double pFirst);
+	public  long SampleModuleFunction2(ref long pFirst) ; /**< \brief Sample function comment */
+	/**
+	 *  <summary>
+	 *  simple method
+	 *  </summary>
+	 *  <remarks>Test remark</remarks>
+	 */
+	void SampleModuleMethod();
+	void SampleModuleMethod2() ; /**< \brief Sample sub comment */
+}
